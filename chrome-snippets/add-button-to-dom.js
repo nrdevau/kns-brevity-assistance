b1cc6664794c390bbe826458f8535d9b4efb8d13
Nate Rogers <nate@nrdev.au>
2025-05-14 07:41:40 +1000

    Improvements to button styling. Working on linking notification window with backend

--- chrome-snippets/add-button-to-dom.js
+++ chrome-snippets/add-button-to-dom.js
@@ -1,11 +1,12 @@
 // Brittle selector to get the checklist container
 let checklistContainer = document.querySelector('#ClientDetails > div:nth-child(1) > div.col-lg-5.col-md-12.col-sm-12.col-xs-12.accordian-group-box > div.portlet.light.bordered.checklist > div > div > div > div > div.panel-headingnew');
-let notifyButton = document.createElement('button');
-notifyButton.onclick = function(){
+let notifyWorkerButton = document.createElement('button');
+notifyWorkerButton.style.cssText = 'background: #3a50b4; background: linear-gradient(90deg, rgba(58, 80, 180, 1) 0%, rgba(253, 29, 29, 1) 50%, rgba(93, 69, 252, 1) 100%);color: white;border: unset;padding: 2px;float: right;'});
+notifyWorkerButton.onclick = function(){
     openWorkerNotificationWindow(); return false;
   };
-notifyButton.appendChild(document.createTextNode("Notify Worker"));
-checklistContainer.append(notifyButton);
+notifyWorkerButton.appendChild(document.createTextNode("Notify Worker"));
+checklistContainer.append(notifyWorkerButton);
 let notifyClientButton = document.createElement('button');
 notifyClientButton.onclick = function(){
     alert('Fire notify client window - todo'); return false;
